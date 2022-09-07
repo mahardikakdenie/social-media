89cf2923da5af8aef5ea6bc0a31842e9aeb10adf
MahardikaKDenie <dikamahar884@gmail.com>
2022-09-07 23:55:41 +0700

    fix navbar

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -2,26 +2,6 @@
   <v-app-bar app color="primary" dark>
     <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
 
-    <div class="d-flex align-center">
-      <v-img
-        alt="Vuetify Logo"
-        class="shrink mr-2"
-        contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-        transition="scale-transition"
-        width="40"
-      />
-
-      <v-img
-        alt="Vuetify Name"
-        class="shrink mt-1 hidden-sm-and-down"
-        contain
-        min-width="100"
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-        width="100"
-      />
-    </div>
-
     <v-spacer></v-spacer>
 
     <v-btn
